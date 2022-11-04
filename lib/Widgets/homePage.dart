23a8e954f4cbb89bdf0511a429cca2e3414229b2
Gabriel Cavalcante <gg25gg26@hotmail.com>
2022-11-04 17:43:12 -0300

    adjustiments

--- lib/Widgets/homePage.dart
+++ lib/Widgets/homePage.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'highlight.dart';
 import 'videoCard.dart';
 import 'categorie.dart';
+import 'videoRegistrationScreen.dart';
+import 'rowCategories.dart';
 
 class homePage extends StatelessWidget {
   const homePage({Key? key}) : super(key: key);
@@ -9,7 +11,17 @@ class homePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: () {}, child:const Icon(Icons.add),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const registrationScreen()
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
       appBar: AppBar(
         backgroundColor: Colors.black87,
         title: const Text(
@@ -27,41 +39,27 @@ class homePage extends StatelessWidget {
             children: [
               const highlight(
                   url:
-                  'https://img.youtube.com/vi/DotnJ7tTA34/maxresdefault.jpg'),
-              Container(
-                padding: const EdgeInsets.only(top: 10),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: const [
-                      categories(texto: 'Ação', cor: Colors.purpleAccent),
-                      categories(texto: 'Aventura', cor: Colors.redAccent),
-                      categories(texto: 'Romance', cor: Colors.blueAccent),
-                      categories(texto: 'Ficção', cor: Colors.green),
-                      categories(texto: 'Terror', cor: Colors.yellow),
-                    ],
-                  ),
-                ),
-              ),
+                      'https://img.youtube.com/vi/DotnJ7tTA34/maxresdefault.jpg'),
+              const rowCategories(),
               Container(
                 padding: EdgeInsets.only(top: 5),
                 child: Column(
                   children: const [
                     videoCard(
                         url:
-                        'https://img.youtube.com/vi/dD264ZjfKlk/maxresdefault.jpg',
+                            'https://img.youtube.com/vi/dD264ZjfKlk/maxresdefault.jpg',
                         categorie:
-                        categories(texto: 'Terror', cor: Colors.yellow)),
+                            categories(texto: 'Terror', cor: Colors.yellow)),
                     videoCard(
                         url:
-                        'https://img.youtube.com/vi/i6avfCqKcQo/maxresdefault.jpg',
+                            'https://img.youtube.com/vi/i6avfCqKcQo/maxresdefault.jpg',
                         categorie:
-                        categories(texto: 'Ficção', cor: Colors.green)),
+                            categories(texto: 'Ficção', cor: Colors.green)),
                     videoCard(
                         url:
-                        'https://img.youtube.com/vi/i6avfCqKcQo/maxresdefault.jpg',
+                            'https://img.youtube.com/vi/i6avfCqKcQo/maxresdefault.jpg',
                         categorie:
-                        categories(texto: 'Ficção', cor: Colors.green))
+                            categories(texto: 'Ficção', cor: Colors.green))
                   ],
                 ),
               ),
---
+++ lib/Widgets/rowCategories.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'categorie.dart';
+
+class rowCategories extends StatelessWidget {
+  const rowCategories({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 10),
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: const [
+            categories(texto: 'Ação', cor: Colors.purpleAccent),
+            categories(texto: 'Aventura', cor: Colors.redAccent),
+            categories(texto: 'Romance', cor: Colors.blueAccent),
+            categories(texto: 'Ficção', cor: Colors.green),
+            categories(texto: 'Terror', cor: Colors.yellow),
+          ],
+        ),
+      ),
+    );
+  }
+}
